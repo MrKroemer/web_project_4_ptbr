a21e43d59683dbaa433207e5b55857d727af91df
mrkroemer <flahrodrigues@yahoo.com.br>
2023-02-22 23:11:51 -0300

    subindo parte do projeto

---
+++ js/script2.js
@@ -0,0 +1,40 @@
+function startModal(classModal) {
+  const modal = document.querySelector('.popup');
+  modal.classList.add('display');
+  modal.addEventListener("click", function (evt) {
+    if (
+      evt.target.id == classModal ||
+      evt.target.className == 'popup__close' ||
+      evt.target.className == 'popup__salvar'
+    ) {
+      modal.classList.remove('display');
+    }
+  });
+}
+
+const button = document.querySelector('.profile__edit');
+button.addEventListener('click', function () {
+  startModal('popup');
+});
+
+const formElement = document.querySelector('.popup__form');
+
+function editProfile(evt) {
+  evt.preventDefault();
+
+  //const title = document.querySelector('profile__author');
+  //const subtitle = document.querySelector('.profile__subtitle');
+  const nameInput = document.querySelector('.popup__input_name');
+  const aboutInput = document.querySelector('popup__input_about');
+
+
+
+  //const nameValue = nameInput.value;
+  //const aboutValue = aboutInput.value;
+
+  //title.textContent = nameValue;
+  //subtitle.textContent =  aboutValue;
+}
+
+
+formElement.addEventListener('submit', handleProfileFormSubmit);
